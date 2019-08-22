76fa8734ed086cc137029506aed1be8e88689431
wangjie <wangjiepyer@163.com>
2019-08-22 11:52:49 +0800

    two-sum second ac

--- two-sum.py
+++ two-sum.py
@@ -1,7 +1,14 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}
+        for i in range(len(nums)):
+            if target - nums[i] not in dic:
+                dic[nums[i]] = i
+            else:
+                return [dic[target - nums[i]], i]
+        return []
